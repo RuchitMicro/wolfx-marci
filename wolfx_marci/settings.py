412d9ea42f3b2a72d7b9d740e61b15018091b8a4
Ruchit Micro <ruchitkharwa@gmail.com>
2026-06-15 19:54:00 +0530

    Update settings.py

--- wolfx_marci/settings.py
+++ wolfx_marci/settings.py
@@ -29,6 +29,10 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
 
+    # Celery
+    'django_celery_beat',
+    'django_celery_results',
+
     # Project apps
     'api',
     'config',
